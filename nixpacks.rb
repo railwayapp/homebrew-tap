2031385e97c1aa2a06d766d4a84c0c6719518394
Jake Runzer <jakerunzer@gmail.com>
2022-04-13 14:41:56 -0700

    Update nixpacks.rb

--- nixpacks.rb
+++ nixpacks.rb
@@ -1,7 +1,7 @@
 class Nixpacks < Formula
     desc " App source + Nix packages + Docker = Image "
     homepage "https://github.com/railwayapp/nixpacks"
-    url "https://github.com/extrawurst/gitui/releases/download/v0.9.0/gitui-mac.tar.gz"
+    url "https://github.com/extrawurst/gitui/releases/download/v0.0.0/gitui-mac.tar.gz"
     sha256 "fb95ff4cb75612788127989f93be08cb6d52e1181b7d1738f2eac26e0c2ac323"
     version "v0.0.0"
   
