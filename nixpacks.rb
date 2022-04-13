483a14a2bc6c4d615681b5bb7e96706887dd873c
Jake Runzer <jakerunzer@gmail.com>
2022-04-13 14:54:21 -0700

    Update nixpacks.rb

--- nixpacks.rb
+++ nixpacks.rb
@@ -1,9 +1,9 @@
 class Nixpacks < Formula
     desc "App source + Nix packages + Docker = Image "
     homepage "https://github.com/railwayapp/automated-rust-release-example"
-    url "https://github.com/railwayapp/cli/releases/download/v1.4.1/railway_1.4.1_darwin_amd64.tar.gz"
+    url "https://github.com/railwayapp/cli/releases/download/v0.0.0/railway_1.4.1_darwin_amd64.tar.gz"
     sha256 "b9b14eeffa146fba3db2b6acc8a724b0fc0d383c77105df1cf11a3fa3cb8f025"
-    version "0.0.26"
+    version "0.0.0"
   
     def install
       bin.install "nixpacks"
