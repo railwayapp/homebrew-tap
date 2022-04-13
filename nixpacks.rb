2c9272201c7a2cd325634aa9e05bfb5e8545e9c9
Jake Runzer <jakerunzer@gmail.com>
2022-04-13 14:51:27 -0700

    Update nixpacks.rb

--- nixpacks.rb
+++ nixpacks.rb
@@ -1,6 +1,6 @@
 class Nixpacks < Formula
     desc "App source + Nix packages + Docker = Image "
-    homepage "https://github.com/railwayapp/nixpacks"
+    homepage "https://github.com/railwayapp/automated-rust-release-example"
     url "https://github.com/railwayapp/automated-rust-release-example/releases/download/v0.0.26/automated-rust-release-example-v0.0.26-macos.tar.gz"
     sha256 "b9b14eeffa146fba3db2b6acc8a724b0fc0d383c77105df1cf11a3fa3cb8f025"
     version "0.0.26"
