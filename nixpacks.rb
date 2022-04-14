97c5b595261335118f0d505491ca495d187d63c6
Jake Runzer <jakerunzer@gmail.com>
2022-04-14 07:46:03 -0700

    Update nixpacks.rb

--- nixpacks.rb
+++ nixpacks.rb
@@ -1,5 +1,5 @@
 class Nixpacks < Formula
-    desc "App source + Nix packages + Docker = Image "
+    desc "App source + Nix packages + Docker = Image"
     homepage "https://github.com/railwayapp/automated-rust-release-example"
     url "https://github.com/railwayapp/nixpacks/releases/download/v0.0.1/nixpacks-v0.0.1-macos.tar.gz"
     sha256 "b653cb43e8a9f334d07fe03fc7b5233c5bbb93460c0a0647b915f2c0bae16764"
