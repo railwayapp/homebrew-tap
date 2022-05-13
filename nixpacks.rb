e2a73d359b52279cc5f58a7b112f0bdd31f54f66
Jake Runzer <jakerunzer@gmail.com>
2022-05-13 01:01:07 -0700

    Update nixpacks.rb

--- nixpacks.rb
+++ nixpacks.rb
@@ -1,9 +1,9 @@
 class Nixpacks < Formula
     desc "App source + Nix packages + Docker = Image"
     homepage "https://github.com/railwayapp/nixpacks"
-    url "https://github.com/railwayapp/nixpacks/releases/download/v0.0.11/nixpacks-v0.0.11-darwin_amd64.tar.gz"
+    url "https://github.com/railwayapp/nixpacks/releases/latest/download/nixpacks-v0.0.15-x86_64-apple-darwin.tar.gz"
     sha256 "acc982fab8bfdb94ea5a309068abd52c41ab8c9f5d29b2bb40826236718c82b2"
-    version "0.0.11"
+    version "0.0.15"
   
     def install
       bin.install "nixpacks"
