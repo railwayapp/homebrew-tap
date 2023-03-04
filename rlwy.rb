4ccede341e8b1c81f292245a4d3d950153c3d68f
nebulatgs <nebula@railway.app>
2023-03-03 23:27:54 -0500

    update to 3.0.1

--- rlwy.rb
+++ rlwy.rb
@@ -1,42 +1,42 @@
-class Rlwy < Formula
+class railway < Formula
   desc "Develop and deploy code with zero configuration"
   homepage "https://railway.app"
   version "3.0.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/railwayapp/cli/releases/download/v3.0.0/rlwy-v3.0.0-aarch64-apple-darwin.tar.gz"
-      sha256 "02fd564787cc2f61770b83f2864be711d3dc9b16e093b2dc1dc2ec00a18e4f2b"
+      url "https://github.com/railwayapp/cli/releases/download/v3.0.1/railway-v3.0.1-aarch64-apple-darwin.tar.gz"
+      sha256 "071dd28575e7accd2bbb9232f0e796e8d7848898539158a5e2fb820beeda4cf8"
 
       def install
-        bin.install "rlwy" => "railway"
+        bin.install "railway"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/railwayapp/cli/releases/download/v3.0.0/rlwy-v3.0.0-x86_64-apple-darwin.tar.gz"
-      sha256 "f9fd62164926458b760553e8d96760b4622e787ad71b2cf0a51c68cd99f319be"
+      url "https://github.com/railwayapp/cli/releases/download/v3.0.1/railway-v3.0.1-x86_64-apple-darwin.tar.gz"
+      sha256 "2a6fb300600d37c6a42cab5043efb6dea2db812bd6945e02491807d71e869998"
 
       def install
-        bin.install "rlwy" => "railway"
+        bin.install "railway"
       end
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/railwayapp/cli/releases/download/v3.0.0/rlwy-v3.0.0-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "59bfb99b77b318a03e30cac6d210b28b7175784ce86828d39ec4338079cbc01d"
+      url "https://github.com/railwayapp/cli/releases/download/v3.0.1/railway-v3.0.1-x86_64-unknown-linux-musl.tar.gz"
+      sha256 "f22bdebfd2bf86eff18b51fdba5effa103a382beb48f07740dcb3a043bc342cf"
 
       def install
-        bin.install "rlwy" => "railway"
+        bin.install "railway"
       end
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/railwayapp/cli/releases/download/v3.0.0/rlwy-v3.0.0-aarch64-unknown-linux-musl.tar.gz"
-      sha256 "22c120b56ad60a58dfee782c5816bc8fcf723668a92e839bd0f80c3052c7b95a"
+      url "https://github.com/railwayapp/cli/releases/download/v3.0.1/railway-v3.0.1-aarch64-unknown-linux-musl.tar.gz"
+      sha256 "61d990c45d3973e7a8396af6168a05bbe273c79db02225365d8b61889b70bc23"
 
       def install
-        bin.install "rlwy" => "railway"
+        bin.install "railway"
       end
     end
   end
